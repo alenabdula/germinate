82e98deaa69c02fa968e6ce0e21cac8553f24ef0
Alen Abdula <alenabdula@gmail.com>
2015-05-08 16:47:17 -0400

    Build Updated, watch not working

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 //
-//  Germinate Build Script, Version 0.0.1 Alpha
+//  Germinate Build Script, Version 0.0.2 Alpha
 //
 /* Gulp */
 var gulp = require('gulp');
@@ -7,9 +7,7 @@ var gulp = require('gulp');
 /* SCSS */
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
-
-/* Critical CSS */
-var critical = require('critical');
+var sourcemaps = require('gulp-sourcemaps');
 
 /* Utilities */
 var concat = require('gulp-concat');
@@ -21,9 +19,6 @@ var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var reporter = require('jshint-stylish');
 
-/* Notify */
-var notify = require('gulp-notify');
-
 /* Images */
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
@@ -31,72 +26,21 @@ var pngquant = require('imagemin-pngquant');
 /* BrowserSync */
 var browserSync = require('browser-sync');
 
-/* Google PageSpeed Insights */
-var psi = require('psi');
-var key = '';
-var url = 'http://germinate.com';
-
-/* PageSpeed Insights, Mobile */
-gulp.task('mobile', function () {
-    return psi(url, {
-        // key: key
-        nokey: 'true',
-        strategy: 'mobile',
-    }, function (err, data) {
-        console.log(data.score);
-        console.log(data.pageStats);
-    });
-});
-
-/* PageSpeed Insights, Desktop */
-gulp.task('desktop', function () {
-    return psi(url, {
-        // key: key,
-        nokey: 'true',
-        strategy: 'desktop',
-    }, function (err, data) {
-        console.log(data.score);
-        console.log(data.pageStats);
-    });
-});
-
 /* SCSS */
 gulp.task('scss', function () {
-    return sass('src/scss/app.scss', {style:'compressed'})
+    return sass('src/scss/app.scss', {
+            style:'compressed',
+            sourcemap: true
+        })
         .on('error', function (err) {
             console.error('Error!', err.message);
         })
+        .pipe(sourcemaps.write())
         .pipe(autoprefixer('last 5 version'))
         .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('public/css/'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(size({title: 'Styles ==>'}));
-});
-
-/* Critical CSS */
-gulp.task('critical', function() {
-    critical.generateInline({
-        // Your base directory
-        base: './',
-        // HTML source
-        // html: '<html>...</html>',
-        // HTML source file
-        src: 'public/index.html',
-        // Your CSS Files (optional)
-        css: ['public/css/app.min.css'],
-        // Viewport width
-        width: 1300,
-        // Viewport height
-        height: 900,
-        // Target for final HTML output
-        // htmlTarget: 'index-critical.html',
-        // Target for generated critical-path CSS (which we inline)
-        styleTarget: 'public/css/critical.css',
-        // Minify critical-path CSS when inlining
-        minify: true,
-        // Extract inlined styles from referenced stylesheets
-        extract: true
-    });
 });
 
 /* JavaScript */
@@ -147,8 +91,9 @@ gulp.task('browser-sync-reload', function () {
 });
 
 /* Watch */
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['scss']);
-    gulp.watch(['src/javascript/*.js', 'src/javascript/**/*.js'], ['javascript']);
-    gulp.watch(['public/*.html'], ['browser-sync-reload']);
-});
+//
+//gulp.task('default', ['browser-sync'], function () {
+//    gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['scss']);
+//    gulp.watch(['src/javascript/*.js', 'src/javascript/**/*.js'], ['javascript']);
+//    gulp.watch(['public/*.html'], ['browser-sync-reload']);
+//});
