04e8d7438642b1e7139e044982b5435863658dda
Alen Abdula <alenabdula@gmail.com>
2015-09-06 11:46:30 -0400

    Updated app.js

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,11 +1,45 @@
-// (function($){
-//     $(document).ready(function(){
-//         console.log('ready');
-//     });
-//     $(window).load(function(){
-//         console.log('load');
-//     });
-//     $(window).resize(function(){
-//         console.log('resize');
-//     });
-// })(window.jQuery);
+
+(function($) {
+  
+  var AppName = {
+    
+    width : $(window).width(),
+    
+    height : $(window).height(),
+    
+    /* https://github.com/louisremi/jquery-smartresize */
+    resize: function(a, b) {
+      onresize = function() {
+        clearTimeout(b);
+        b = setTimeout(a, 100);
+      };
+      return a;
+    },
+    
+    on_resize : function() {
+      this.width = $(window).width();
+      this.height = $(window).height();
+      console.log('Resized!!!', 'Width: ' + this.width, 'Height: ' + this.height);
+    },
+    
+    init : function() {
+      console.log('Initialized!!!', 'Width: ' + this.width, 'Height: ' + this.height);
+    },
+    
+  };
+  
+  /* Document Ready  */
+  $(document).on('ready', function() {
+    
+    AppName.init();
+    
+  });
+  
+  /* Window Resize */
+  AppName.resize(function() {
+    
+    AppName.on_resize();
+    
+  });
+  
+})(window.jQuery);
