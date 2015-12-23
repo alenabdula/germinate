fa6a4b99fe2e326c36b49ce1a05b71d47ee681b8
Alen Abdula <alenabdula@gmail.com>
2015-12-23 12:16:23 -0500

    WIP, started dev branch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,87 +1,47 @@
 /* Helpers */
 function consoleEventReporter(event) {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
-/* Gulp */
 var gulp = require('gulp');
-
-/* SCSS */
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-
-/* Utilities */
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var size = require('gulp-size');
-
-/* JavaScript */
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var stylish = require('jshint-stylish');
-
-/* BrowserSync */
 var browserSync = require('browser-sync');
 
-gulp.task('scss', function () {
-  return gulp.src('src/scss/app.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass({ outputStyle: 'expanded' }).on( 'error', sass.logError ))
-  .pipe(sourcemaps.write())
-  .pipe(autoprefixer({browsers: ['last 3 versions', '> 5%', 'Firefox ESR']}))
-  .pipe(gulp.dest('public/css/'))
-  .pipe(browserSync.reload({ stream: true }))
-  .pipe(size({title: 'Styles ==>'}));
-});
-
-/* JavaScript */
-gulp.task('javascript', function () {
-  return gulp.src([
-    './src/javascript/vendor/**/*.js',
-    './src/javascript/includes/**/*.js',
-    './src/javascript/app.js',
-  ])
-  .pipe(concat('app.js'))
-  .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jshint.reporter('fail'))
-  .pipe(uglify())
-  .pipe(rename({
-    basename: "app",
-    suffix:'.min',
-    extname: ".js"
-  }))
-  .pipe(gulp.dest('./public/js/'))
-  .pipe(size({title: 'JavaScript ==>'}));
+/* Style */
+gulp.task('scss', function() {
+  return gulp.src('./src/scss/master.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on( 'error', sass.logError ))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({browsers: ['last 3 versions', '> 5%', 'Firefox ESR']}))
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(browserSync.reload({ stream: true }))
+  ;
 });
 
 /* BrowserSync */
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function() {
   browserSync({
-        // Option 1
-        server: {
-          baseDir: 'public',
-      },
-        // Option 2
-        // proxy: "germinate.dev",
-    });
+  // Option 1
+  server: {
+    baseDir: 'public',
+  },
+  // Option 2
+  // proxy: "germinate.dev",
+  });
 });
 
 /* BrowserSync Manual Reload */
-gulp.task('browser-sync-reload', function () {
+gulp.task('browser-sync-reload', function() {
   browserSync.reload();
 });
 
 /* Watch */
-gulp.task('default', ['browser-sync'], function () {
-    /* SCSS */
-    gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['scss'])
-        .on('change', function(event) { consoleEventReporter(event); });
-    /* JavaScript */
-    gulp.watch(['src/javascript/*.js', 'src/javascript/**/*.js'], ['javascript', 'browser-sync-reload'])
-        .on('change', function(event) { consoleEventReporter(event); });
-    /* HTML */
-    gulp.watch(['public/*.html'], ['browser-sync-reload'])
-        .on('change', function(event) { consoleEventReporter(event); });
+gulp.task('default', ['browser-sync'], function() {
+  /* Style */
+  gulp.watch(['./src/scss/*.scss', './src/scss/**/*.scss'], ['scss'])
+    .on('change', function(event) { consoleEventReporter(event);});
+
 });
--- README.md
+++ README.md
@@ -1,20 +1,4 @@
 ##Germinate##
-
-Project Boilerplate, under development.
-
-###Getting Started###
-
-**Tools & Requirements**
-
-In order to use Gulp, our build system/script, you will need to install following dependencies:
-
-+ [NodeJS](http://nodejs.org), Also see [Installing Node.js via package manager](https://github.com/joyent/node/wiki/installing-node.js-via-package-manager)
-+ From root folder run following commands:
-  + `npm install -g gulp` installs Gulp globally
-  + `npm install` installs our development dependencies from `composer.json` [file](https://github.com/alenabdula/germinate/blob/master/package.json).
-  + And finally run `gulp` to start [default Gulp task](https://github.com/alenabdula/germinate/blob/master/Gulpfile.js#L77)
-  + To run Gulp tasks individually you would run `gulp <task name>` ex. `gulp scss` or `gulp javascript`.
-
 ###Inspired & Attributed###
 
 - [Normalize.css](http://necolas.github.io/normalize.css)
