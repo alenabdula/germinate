cc20a701645bbb24d6e78aa49c38cf2b6d770b95
Alen Abdula <alenabdula@gmail.com>
2016-05-13 21:18:03 -0400

    WIP

--- public/js/master-es6.js
+++ public/js/master-es6.js
@@ -14,7 +14,7 @@
   }
 
   /**
-   *  VueJS
+   *  VueJS example, external resource
    */
   const vm = new Vue({
     el: 'body',
@@ -24,13 +24,41 @@
   });
 
   /**
-   *  jQuery
+   *  jQuery example, external resource
    */
-  let $body = $('body');
-  console.log($body);
+  const Germinate = {
+    width: $(window).width(),
+    height: $(window).height(),
+    onResize: function (a, b) {
+      onresize = function () {
+        clearTimeout(b);
+        b = setTimeout(a, 100);
+      };
+      return a;
+    },
+    resize: function () {
+      this.width = $(window).width();
+      this.height = $(window).height();
+      console.log("%cResized!", "color: #A4CE39; font-size: 2rem;");
+      console.info('Width: ' + this.width);
+      console.info('Height: ' + this.height);
+    },
+    ready: function () {
+      console.log("%cDocument Ready!", "color: #A4CE39; font-size: 2rem;");
+      console.info('Width: ' + this.width);
+      console.info('Height: ' + this.height);
+    }
+  };
+
+  $(document).ready(function () {
+    Germinate.ready();
+  });
+  Germinate.onResize(function () {
+    Germinate.resize();
+  });
 
   /**
-   *  Test module
+   *  Test.js, local module
    */
   let foo = new Test();
   console.log(foo.init());
--- src/javascript/master-es6.js
+++ src/javascript/master-es6.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import { Test } from './modules/Test';
 
 /**
- *  VueJS, external resource
+ *  VueJS example, external resource
  */
 const vm = new Vue({
   el: 'body',
@@ -13,10 +13,34 @@ const vm = new Vue({
 });
 
 /**
- *  jQuery, external resource
+ *  jQuery example, external resource
  */
-let $body = $('body');
-console.log($body);
+const Germinate = {
+  width: $(window).width(),
+  height: $(window).height(),
+  onResize: function(a, b) {
+    onresize = function() {
+      clearTimeout(b);
+      b = setTimeout(a, 100);
+    };
+    return a;
+  },
+  resize: function() {
+    this.width = $(window).width();
+    this.height = $(window).height();
+    console.log("%cResized!", "color: #A4CE39; font-size: 2rem;");
+    console.info('Width: ' + this.width);
+    console.info('Height: ' + this.height);
+  },
+  ready: function() {
+    console.log("%cDocument Ready!", "color: #A4CE39; font-size: 2rem;");
+    console.info('Width: ' + this.width);
+    console.info('Height: ' + this.height);
+  }
+};
+
+$(document).ready( function() { Germinate.ready(); });
+Germinate.onResize( function() { Germinate.resize(); });
 
 /**
  *  Test.js, local module
